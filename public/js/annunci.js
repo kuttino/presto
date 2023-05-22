c3078c54a6defdc30d57af080219149ccf6a4c5a
cyberdelia-dev <cyberdeliaeu@gmail.com>
2023-05-22 21:48:42 +0200

    fileteringAndSorting

--- public/js/annunci.js
+++ public/js/annunci.js
@@ -1,29 +1,4 @@
 
-
-
-/*
-
-    <div class="card h-100 position-relative">
-
-        <span class="position-absolute top-0 end-0 badge rounded-2 px-4 py-2 text-uppercase bg-sell">Sell</span>
-
-        <img src="https://picsum.photos/id/237/640" class="card-img-top" alt="...">
-        <div class="card-body p-4">
-            <h6 class="card-subtitle text-primary fw-semibold fs-4">€120.12</h6>
-            <h5 class="card-title display-4">Huawei X5</h5>
-            <p class="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        <div class="card-footer text-muted bg-white d-flex justify-content-around">
-            <a href="#" class="text-primary text-decoration-none"><i class="bi bi-tag-fill me-2"></i>Elettronica</a>
-            <a href="#" class="text-primary text-decoration-none"><i class="bi bi-calendar-fill me-2"></i>27/3/2023</a>
-        </div>
-    </div>
-*/
-
-
-
-
-
 function createAnnouncementCard(announcement) {
 
     const date = new Date(announcement.createdAt);
@@ -57,6 +32,13 @@ function createAnnouncementCard(announcement) {
 async function populateAnnouncements(announcements) {
 
     const cardsRow = document.getElementById('cardsRow');
+
+    // elimino tutti i child precedenti dalla riga (se presenti)
+    while(cardsRow.hasChildNodes()) {
+        cardsRow.removeChild(cardsRow.firstChild);
+    }
+
+    // creo e appendo le nuove card
     announcements.forEach((announcement) => {
 
         // div class="col-12 col-md-6 col-lg-4 mb-4"
@@ -69,16 +51,128 @@ async function populateAnnouncements(announcements) {
     });
 }
 
+
+
+
 async function readAllAnnouncements() {
     const response = await fetch("../../server/api/annunci.json");
     const announcements = await response.json();
     return announcements;
 }
 
+function filterAnnouncements(announcements, options) {
 
+    let filteredAnnouncements = announcements.filter((announcement) => {
+
+        let isAnnouncementRequired = true;
+
+        if(options.searchInputValue.length > 0) {
+            isAnnouncementRequired = announcement.name.toLowerCase().includes(options.searchInputValue.toLowerCase());
+        }
+
+        if(isAnnouncementRequired && options.categorySelectValue.length > 0) {
+            isAnnouncementRequired = announcement.category.toLowerCase() == options.categorySelectValue.toLowerCase();
+        }
+
+        if(isAnnouncementRequired && options.minPriceInputValue.length > 0) {
+            isAnnouncementRequired = parseFloat(announcement.price) >= parseFloat(options.minPriceInputValue);
+        }
+
+        if(isAnnouncementRequired && options.maxPriceInputValue.length > 0) {
+            isAnnouncementRequired = parseFloat(announcement.price) < parseFloat(options.maxPriceInputValue);
+        }
+
+        return isAnnouncementRequired;
+    });
+
+    return filteredAnnouncements;
+}
+
+function sortAnnouncements(announcements, sortSelectValue) {
+
+    // ordino gli annunci
+    switch(sortSelectValue) {
+        case 'descByDate':
+            announcements.sort((left, right) => {
+                return parseInt(right.createdAt) - parseInt(left.createdAt);
+            });
+            break;
+        case 'ascByDate':
+            announcements.sort((left, right) => {
+                return parseInt(left.createdAt) - parseInt(right.createdAt);
+            });
+            break;
+        case 'ascByPrice':
+            announcements.sort((left, right) => {
+                return parseFloat(left.price) - parseFloat(right.price);
+            });
+            break;
+        case 'descByPrice':
+            announcements.sort((left, right) => {
+                return parseFloat(right.price) - parseFloat(left.price);
+            });
+            break;
+        case 'ascByAlpha':
+            announcements.sort((left, right) => {
+                return left.name.toLowerCase().localeCompare(right.name.toLowerCase());
+            });
+            break;
+        case 'descByAlpha':
+            announcements.sort((left, right) => {
+                return right.name.toLowerCase().localeCompare(left.name.toLowerCase());
+            });
+            break;
+    }
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
+
+    const searchInput = document.getElementById('searchInput');
+    const categorySelect = document.getElementById('categorySelect');
+    const minPriceInput = document.getElementById('minPriceInput');
+    const maxPriceInput = document.getElementById('maxPriceInput');
+    const sortSelect = document.getElementById('sortSelect');
+    const filteringForm = document.getElementById('filteringForm');
+
+
+
+    filteringForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        /*
+        const searchInputValue = searchInput.value;
+        const categorySelectValue = categorySelect.value;
+        const minPriceInputValue = minPriceInput.value;
+        const maxPriceInputValue = maxPriceInput.value;
+        */
+
+        const announcements = await readAllAnnouncements();
+
+
+        // filtro l'array di annunci
+        const options = {
+            searchInputValue: searchInput.value,
+            categorySelectValue: categorySelect.value,
+            minPriceInputValue: minPriceInput.value,
+            maxPriceInputValue: maxPriceInput.value
+        };
+        let filteredAnnouncements = filterAnnouncements(announcements, options);
+
+
+        // ordino l'array di annunci filtrato
+        const sortSelectValue = sortSelect.value;
+        sortAnnouncements(filteredAnnouncements, sortSelectValue);
+        
+
+
+        await populateAnnouncements(filteredAnnouncements);
+    });
+
+
 
     const announcements = await readAllAnnouncements();
     await populateAnnouncements(announcements);
 });
+
+
+
